74202940ac8316a41cb29ee7bd951743a3a4cf22
navabhi22 <navabhi22@gmail.com>
2022-02-21 21:15:54 +0530

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -7,12 +7,9 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("This problem simulates a Snake and Ladder Game");
-            Console.WriteLine(" Single Player Game, Player Start at zero ");
-            int Start = 0;
-            int End = 100;
-            int ROll = DiceRoll.RollTheDice();
-            Console.WriteLine("Checking for options Ladder, Snake or NoPlay");
-            ExactWinning.GamePlay();
+            Console.WriteLine(" Two Player Game, Player Start at zero ");
+        
+            TwoPlayer.TwoPlayerGame();
         }
     }
 }

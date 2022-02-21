e5e2ab7908c6bbe080b01511e4cbbe452af747fc
navabhi22 <navabhi22@gmail.com>
2022-02-21 13:18:00 +0530

    Added Class to Start from zero if position < 0 reached

---
+++ StartAtZero.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class StartAtZero
+    {
+        public static int[] GamePlay()
+        {
+            int Position = 0;
+            int counter = 0;
+            while (Position < 100)
+
+            {
+                if (Position <= 0)
+                {
+                    Position = 0;
+                    Position = Play.GamePlay(Position);
+                    //Console.WriteLine(Position);
+                }
+                else
+                {
+                    Position = Play.GamePlay(Position);
+                    //Console.WriteLine(Position);
+                }
+                counter++;
+            }
+            int[] arr = new int[2];
+            arr[0] = counter;
+            arr[1] = Position;
+            return arr;
+        }
+    }
+}

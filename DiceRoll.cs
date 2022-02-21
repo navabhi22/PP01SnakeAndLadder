78dd806d8359a380bd4260e4f3a4de72a9dc7717
navabhi22 <navabhi22@gmail.com>
2022-02-21 12:48:55 +0530

    Added Class to get a Dice Roll

---
+++ DiceRoll.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class DiceRoll
+    {
+        public static int RollTheDice()
+        {
+            Random random = new Random();
+            int roll = random.Next(1, 7);
+            return roll;
+        }
+    }
+}

4a9a8edaf03337bc75019104a005a567fb6b4c3a
navabhi22 <navabhi22@gmail.com>
2022-02-21 12:58:24 +0530

    Added Class to Check options Ladder Snake or NoPlay

---
+++ CheckOption.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class CheckOption
+    {
+        public static int Option()
+        {
+            Random random = new Random();
+            int option = random.Next(0, 3);
+            return option;
+        }
+    }
+}

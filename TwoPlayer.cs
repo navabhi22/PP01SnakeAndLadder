713d804726780774b2e29d0724d59fb59c655881
navabhi22 <navabhi22@gmail.com>
2022-02-21 21:13:39 +0530

    Added class for two player win

---
+++ TwoPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class TwoPlayer
+    {
+        public static void TwoPlayerGame()
+        {
+            int Player1Position=0;
+            int Player2Position=0;
+            Console.WriteLine("Player1 first roll the dice");
+            while (Player1Position <100 && Player2Position <= 100)
+            {                
+                Player1Position = SnakeAndLadder.TwoPlayerGame.GamePlay(Player1Position);
+                Console.WriteLine("Player 1 position is " + Player1Position);
+                
+                Player2Position = SnakeAndLadder.TwoPlayerGame.GamePlay(Player1Position);
+                Console.WriteLine("Player 2 position is " + Player2Position);
+                
+            }
+        }
+    }
+}

899bb7c6e16ad632a9bc0b74906c3e4cbde14c2c
navabhi22 <navabhi22@gmail.com>
2022-02-21 21:07:59 +0530

    Added class for two player play

---
+++ TwoPlayerPlay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class TwoPlayerGame
+    {
+        public static int GamePlay(int Position)
+        { 
+            
+                if (Position <= 0)
+                {
+                    Position = 0;
+                    Position = Play.GamePlay(Position);
+                }
+                else
+                {
+                    Position = Play.GamePlay(Position);
+                }
+               
+            
+            return Position;
+        }
+    }
+}

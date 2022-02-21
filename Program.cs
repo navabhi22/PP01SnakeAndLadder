a913f7a11392f4d7b52f12420beadd4e120b3ee5
navabhi22 <navabhi22@gmail.com>
2022-02-21 17:39:44 +0530

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -12,8 +12,7 @@ namespace SnakeAndLadder
             int End = 100;
             int ROll = DiceRoll.RollTheDice();
             Console.WriteLine("Checking for options Ladder, Snake or NoPlay");
-            int[] PositionAndNoOfRoll = StartAtZero.GamePlay();
-            Console.WriteLine("Position: " + PositionAndNoOfRoll[1] + ", Number of Dice Roll " + PositionAndNoOfRoll[0] );
+            ExactWinning.GamePlay();
         }
     }
 }

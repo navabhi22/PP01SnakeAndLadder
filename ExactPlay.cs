8ba3bc5029a21bd8c3ca25d263ec2ea22d07839d
navabhi22 <navabhi22@gmail.com>
2022-02-21 17:38:22 +0530

    Added Class to get Exact play after position 95

---
+++ ExactPlay.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class ExactPlay
+    {
+        public static int GamePlay(int Position, int roll)
+        {
+            int Snake = 0;
+            int Ladder = 2;
+            int NoPlay = 1;
+            int option = CheckOption.Option();
+            if (option == Snake)
+            {
+                Position = Position - roll;
+                Console.WriteLine("snake");
+            }
+            else if (option == Ladder)
+            {
+                Position = Position + roll;
+                Console.WriteLine("Ladder");
+            }
+            else if (option == NoPlay)
+            {
+                Console.WriteLine("NoPlay");
+            }
+
+            return Position;
+        }
+    }
+}

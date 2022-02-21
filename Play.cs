fdc894dfb308fc30507968b87da066db5d600956
navabhi22 <navabhi22@gmail.com>
2022-02-21 13:05:12 +0530

    Added class to Play according to option

---
+++ Play.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Play
+    {
+        public static int GamePlay(int Position)
+        {
+            int Snake = 0;
+            int Ladder = 2;
+            int roll = DiceRoll.RollTheDice();
+
+            int option = CheckOption.Option();
+            if (option == Snake)
+            {
+                Position = Position - roll;
+                Console.WriteLine("snake");
+            }
+            else if (option == Ladder)
+            {
+                Position = Position + roll;
+                Console.WriteLine("Ladder");
+            }
+            else
+            {
+                Console.WriteLine("NoPlay");
+            }
+
+           return Position;
+        }
+    }
+}
